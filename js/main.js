9b575d1db3803994485f59c9659ea305b5991fe0
Theodore Kluge <villa7.kluge5@gmail.com>
2016-05-22 19:07:18 -0400

    ie why

--- js/main.js
+++ js/main.js
@@ -52,6 +52,11 @@
     $(document).on('resize', function() {box.maxLines = Math.ceil($(window).height() / 18);});
 
     box.init = function() {
+
+        if (detectIE() != false) {
+            $('html,body,.lines,.textarea').css('height', '100%');
+        }
+
         box.writeCommentBlock(false);
         //box.choose();
     }
@@ -488,3 +493,29 @@
     }
 
 })(window.box = window.box || {}, jQuery);
+
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
